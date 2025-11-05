421a19a738471bd4bf393bfd39dc9c42df0348a3
Arnaldo Alvarado <arnaldoalvaradoprado@gmail.com>
2025-11-05 23:18:10 +0100

    La suma de los cien primeros numeros

--- prueba.py
+++ prueba.py
@@ -1,1 +1,19 @@
 print("Hola Mundo")
+
+s = sum(range(1, 101))
+print("Suma de los primeros 100 números:", s)
+
+total = 0
+for i in range(1, 101):
+    total += i
+print("Suma de los primeros 100 números usando for:", total)
+
+i = 1
+total_while = 0
+while i <= 100:
+    total_while += i
+    i += 1
+print("Suma de los primeros 100 números usando while:", total_while)
+
+
+
